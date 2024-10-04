841d27f3d11f58f94ab2c9a8c7a0bf98fa606ef6
Judah Mendez <judysubscribes@gmail.com>
2024-10-04 16:14:19 -0400

    configures Results components to accept api data and adds initial msg and no results msg

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,20 +1,10 @@
 //import './Result.css';
-//import { useState } from 'react';
-//import { useEffect } from 'react';
-const img = require('../resources/e_e-albumArt.jpeg'); //note: webpack needs local images to be required in rathern than referenced by source alone
 
-function Result() {
-    const result = {
-        src: "example/hyperlink.jpeg", /**This still needs to be applied in the lower code */
-        altText: "alternate text sample",
-        album: 'sample album',
-        artist: 'sample artist',
-        track: 'sample song',
-    }
+function Result({result}) {
 
     return (
       <div>
-        <img src={img} alt={result.alt}/>
+        <img src={result.src} alt={result.altText}/>
         <h2>{result.track}</h2>
         <h3 >{result.artist}</h3>
         <p>{result.album}</p>
@@ -22,4 +12,4 @@ function Result() {
     );
 }
   
-  export default Result;
+export default Result;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,17 +1,20 @@
 //import './SearchResults.css';
 import Result from "./Result";
 
-function SearchResults() {
+function SearchResults({results}) {
   /**For each arr item received from API feed to Result component to render */
-    return (
-      <div>
-        <Result />
-        <Result />
-        <Result />
-        <Result />
-        <Result />
-      </div>
-    );
+  let jsx = [];
+
+  if(!results) {
+    jsx = <div><p>Start a search and start jammming!</p></div>;
+  } else if(!results.length) {
+    jsx = <div><p>Hmmm...we couldn't find anything. Please try a different search.</p></div>
+  } else {
+    results.forEach( (result) => {jsx.push(<Result result={result} />)} ) 
+    jsx = <div>{jsx}</div>;
+  }
+  
+  return jsx;
   }
   
   export default SearchResults;
