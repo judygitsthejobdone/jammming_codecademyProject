30a92ba7b4cafb29316cfad8a94b7c7e55e2b183
Judah Mendez <judysubscribes@gmail.com>
2024-10-07 12:49:38 -0400

    moves tracklist state to App level, adds mock tracks, and adds mock functions for add/remove actions

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { useState } from 'react';
 
 function App() {
   const [results, setResults] = useState(null);
+  const [tracklist, setTracklist] = useState([mockTrack, mockTrack]);
   
   const handleSearch = (q, type) => { 
     searchSpotify(q, type)
@@ -26,8 +27,8 @@ function App() {
         </a>
       </header>
       <SearchBar handleSearch={handleSearch} />
-      <SearchResults results={results} />
-      <Playlist />
+      <SearchResults results={results} setTracklist={setTracklist} />
+      <Playlist tracklist={tracklist} setTracklist={setTracklist} />
       <Footer />
     </div>
   );
@@ -45,3 +46,13 @@ export default App;
     - Track
     - Save to Spotify button
      */
+
+    const mockTrack = {
+      "track":"Get High",
+      "album":"Side Effects EP",
+      "artist":"Chris Travis",
+      "src":"https://i.scdn.co/image/ab67616d00004851ffa4064cb0e135b5e1ca41a2",
+      "altText":"Side Effects EP album art",
+      "uri":"spotify:track:2HEoWCdoCM6JfTaEGGgxXX"
+  }
+  
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,24 +1,31 @@
 //import './Playlist.css';
 import { useState } from 'react';
+import Track from './Track';
 
-function Playlist(props) {
+function Playlist({tracklist, setTracklist}) {
     const [playlistName, setPlaylistName] = useState('');
-    const [tracklist, setTracklist] = useState([]);
+
     function handleNameChange({target}) {
         setPlaylistName(target.value);
+    }
+    function removeTrack({target}) {
+        setTracklist(tracklist.filter(index => index!==target.value))
+        console.log(`Trying to remove playlist index ${target.value} from the tracklist.`)
     }
     function handleSave() {
         //code to save playlist
         //props.savePlaylist(playlistName, tracklist);
+        console.log(`saving playlist ${playlistName} to user's account...`);
     }
     return (
       <div>
         <label htmlFor="playlistName" >Playlist: </label>
         <input type="text" id="playlistName" maxLength={50} value={playlistName} onChange={handleNameChange}></input>
+        {tracklist.map( ( track, index ) => <Track track={track} key={index} index={index} handleClick={removeTrack} buttonLabel="-" /> ) }
         <br />
         <button type='button' onClick={handleSave}>Save Playlist</button>
       </div>
     );
 }
-  
+
   export default Playlist;
